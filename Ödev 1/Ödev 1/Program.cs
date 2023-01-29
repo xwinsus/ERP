9e069273d85660c3fb9d094f38908693d822cde6
xwinsus <pdgbrk@outlook.com>
2023-01-29 18:07:37 +0300

    Ödev 1 (27.01.2023)
    Kullanıcının kaçıncı ay ve hangi yarım kürede olduğunun girdisi ile hangi mevsimde olduğunu yazdıran console kodu.

---
+++ Ödev 1/Ödev 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hangi aydayız? (1-12):");
+            int ay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hangi yarım küredeyiz? (K/G):");
+            string yariküre = Console.ReadLine();
+
+            switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine(yariküre == "K" ? "Kış" : "Yaz");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine(yariküre == "K" ? "Ilkbahar" : "Sonbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine(yariküre == "K" ? "Yaz" : "Kış");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine(yariküre == "K" ? "Sonbahar" : "Ilkbahar");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz ay.");
+                    break;
+            }
+            Console.ReadLine();
+            Main(args);
+        }
+    }
+}
---
+++ Ödev 1/Ödev 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev2 
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaçıncı aydayız? (1-12):");
+            int ay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hangi yarım küredeyiz? (K/G):");
+            string yarimKure = Console.ReadLine();
+
+            if (ay >= 3 && ay <= 5 && yarimKure == "K")
+            {
+                Console.WriteLine("İlkbahar");
+            }
+            else if (ay >= 6 && ay <= 8 && yarimKure == "K")
+            {
+                Console.WriteLine("Yaz");
+            }
+            else if (ay >= 9 && ay <= 11 && yarimKure == "K")
+            {
+                Console.WriteLine("Sonbahar");
+            }
+            else if (ay == 12 || ay == 1 || ay == 2 && yarimKure == "K")
+            {
+                Console.WriteLine("Kış");
+            }
+            else if (ay >= 3 && ay <= 5 && yarimKure == "G")
+            {
+                Console.WriteLine("Sonbahar");
+            }
+            else if (ay >= 6 && ay <= 8 && yarimKure== "G")
+            {
+                Console.WriteLine("Kış");
+            }
+            else if (ay >= 9 && ay <= 11 && yarimKure == "G")
+            {
+                Console.WriteLine("İlkbahar");
+            }
+            else if (ay == 12 || ay == 1 || ay == 2 && yarimKure == "G")
+            {
+                Console.WriteLine("Yaz");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş. Lütfen tekrar deneyin.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
